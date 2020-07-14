f1dc6d14d951cc77f1f59d725ff85ebfac8537cc
Ryan Duff <ryanduff613@gmail.com>
2020-07-14 11:33:25 -0700

    add authentication and routing to sign-in page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import { FiHome } from 'react-icons/fi';
 
 function Header(){
   return (
-    <h1>Help Queue</h1>
+    <React.Fragment>
+      <h1>Help Queue</h1>
+      <ul>
+        <li>
+          <Link to="/"><FiHome /></Link>
+        </li>
+        <li>
+          <Link to="/signin">Sign In</Link>
+        </li>
+      </ul>
+    </React.Fragment>
   );
 }
 
